0efe27d13f919de9986f91d4e67ecc120d8df9e6
originehsan <originehsan@gmail.com>
2026-01-31 00:14:53 +0530

    chnages made

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      // chnages will start here 
       home: isSignedIn ? const HomePage() : const Loginpage(),
     );
   }
